f7ad975451cc348c6d8949743f058ab8d222b601
ofri masad <ofri@deci.ai>
2022-12-25 21:47:34 +0200

    add both matchers mode

--- src/utils/translate_squad_from_base.py
+++ src/utils/translate_squad_from_base.py
@@ -4,6 +4,9 @@ import json
 import re
 
 import os
+
+import numpy as np
+from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 from utils.smart_match import ModelMatcher, CorrelationMatcher
@@ -21,10 +24,19 @@ def hash_original(original_dataset_path):
         for paragraph in paragraphs:
             for qa in paragraph['qas']:
                 assert qa['id'] not in orig_dict
-                orig_dict[qa['id']] = qa
+                orig_dict[qa['id']] = {"qa": qa, "context": paragraph['context']}
 
     return orig_dict
 
+def draw_hist(data: list, title, wc):
+    counts, bins = np.histogram(data, bins=np.arange(40))
+
+    counts_wc, bins_wc = np.histogram(wc, bins=np.arange(40))
+    counts = np.clip(counts / (counts_wc + 0.0000001), 0, 1)
+
+    plt.stairs(counts, bins, fill=True)
+    plt.title(title)
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -38,6 +50,7 @@ if __name__ == "__main__":
     parser.add_argument('--from_en', action='store_true', help='match from english')
     parser.add_argument('--augment', action='store_true', help='match from english')
     parser.add_argument('--correlation', action='store_true', help='use correlation matcher')
+    parser.add_argument('--both', action='store_true', help='use both type of matchers')
 
     opt = parser.parse_args()
 
@@ -51,6 +64,9 @@ if __name__ == "__main__":
     else:
         matcher = ModelMatcher(model_name_or_path=opt.model_path)
 
+    if opt.both:
+        cor_matcher = CorrelationMatcher('bert-base-multilingual-cased')
+
     with open(opt.base_input_path) as json_file:
         full_doc = json.load(json_file)
 
@@ -62,6 +78,12 @@ if __name__ == "__main__":
     data = full_doc['data']
     new_data = []
     multiple = 0
+    cant_find = 0
+    wc_success = []
+    wc_fail = []
+    wc_success_cor = []
+    wc_fail_cor = []
+    wc = []
     require_translation, not_require_translation, success = 0,0,0
 
     from_en = opt.from_en
@@ -103,11 +125,29 @@ if __name__ == "__main__":
                             translated_text = ans['translated_text']
                             translated_offset = ans['translated_offset']
                             new_answer, score = matcher.match(translated_context, original_text)
+                            cor_score = 0
+                            length = len(original_text.split(" "))
+                            # wc.append(length)
+                            # if score > opt.match_thresh:
+                            #     wc_success.append(length)
+                            # else:
+                            #     wc_fail.append(length)
 
-                            if score > opt.match_thresh:
-                                success += 1
+                            # new_answer, score = cor_matcher.match(translated_context, original_text)
+                            # if score > 0.5:
+                            #     wc_success_cor.append(l)
+                            # else:
+                            #     wc_fail_cor.append(l)
+
+                            # if long sentence and failed to match
+                            if opt.both and score < opt.match_thresh and length > 15:
+                                new_answer, cor_score = cor_matcher.match(translated_context, original_text)
+
+                            if score > opt.match_thresh or cor_score > 0.5:
+
                                 translated_start_indices = [_.start() for _ in re.finditer(re.escape(new_answer), translated_context)]
                                 if len(translated_start_indices) == 1:
+                                    success += 1
                                     if opt.augment:
                                         new_item['answers']['text'].append(translated_text)
                                         new_item['answers']['answer_start'].append(translated_start_indices[0] + translated_offset)
@@ -120,7 +160,7 @@ if __name__ == "__main__":
                                 else:
                                     multiple += 1
                             else:
-                                multiple += 1
+                                cant_find += 1
                         else:
                             not_require_translation += 1
                             new_item['answers']['text'].append(ans['text'])
@@ -130,13 +170,21 @@ if __name__ == "__main__":
                     new_data.append(new_item)
 
     print(f'multiple: {multiple}')
-
+    print(f'cant_find: {cant_find}')
     phase = 'dev' if 'dev' in opt.base_input_path else 'train'
     out_dir = opt.output_dir or os.path.dirname(opt.base_input_path)
-    output_path = f'{out_dir}/{phase}_v1.0hf_{opt.lang}_{opt.match_thresh:.2f}{"_enq" if opt.from_en else ""}{"_aug" if opt.augment else ""}.json'
+    output_path = f'{out_dir}/{phase}_v1.0hf_{opt.lang}_{opt.match_thresh:.2f}{"_enq" if opt.from_en else ""}{"_aug" if opt.augment else ""}{"_both" if opt.both else ""}.json'
     print(f'require: {require_translation}')
     print(f'not require {not_require_translation}')
     print(f'success: {success}')
+
+    # wc = np.array(wc)
+    # draw_hist(np.array(wc_success) , "wc_success", wc)
+    # draw_hist(np.array(wc_fail), "wc_fail", wc)
+    # draw_hist(np.array(wc_success_cor) , "wc_success_cor", wc)
+    # draw_hist(np.array(wc_fail_cor) , "wc_fail_cor", wc)
+    # squad_statistics_hf.draw_hist(wc, "wc")
+    # extract_stats(new_data)
     with open(output_path, 'w') as json_out:
         full_doc['data'] = new_data
         full_doc['version'] = 'v1.0'
