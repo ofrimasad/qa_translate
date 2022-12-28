87522912188e5a20fa1c3417a32c452ec0149e52
ofri masad <ofri@deci.ai>
2022-12-28 16:40:48 +0200

    added --both mode

--- scripts/translate_all_squad_from_base.sh
+++ scripts/translate_all_squad_from_base.sh
@@ -3,8 +3,8 @@ export BASEPATH=/home/ofri/qa_translate
 export PYTHONPATH=${BASEPATH}/src
 export DATAPATH=${BASEPATH}/data
 
-for lang in "ar" "de" "el" "es" "hi"
+for lang in "ar" "de" "el" "es" "hi" "ru" "th" "tr" "vi" "zh-CN"
 do
-  python ${PYTHONPATH}/utils/translate_squad_from_base.py ${DATAPATH}/squad/base_datasets/train-v2.0_${lang}_base.json ${lang} ${BASEPATH}/matcher_exp/non_enq_new/train_matcher_${lang} --output_dir ${DATAPATH}/squad/no_enq05_aug/ --match_thresh 0.5;
-  python ${PYTHONPATH}/utils/translate_squad_from_base.py ${DATAPATH}/squad/base_datasets/dev-v2.0_${lang}_base.json ${lang} ${BASEPATH}/matcher_exp/non_enq_new/train_matcher_${lang} --output_dir ${DATAPATH}/squad/no_enq05_aug/ --original_dataset_path ${DATAPATH}/squad/dev-v2.0.json --match_thresh 0.5;
+  python ${PYTHONPATH}/utils/translate_squad_from_base.py ${DATAPATH}/squad/base_v1.1/train-v1.1_${lang}_base_n.json ${lang} ${BASEPATH}/matcher_exp/enq_25_12_2022/train_matcher_${lang} --output_dir ${DATAPATH}/squad/enq_25_12_2022/ --match_thresh 0.05 --from_en --both;
+  python ${PYTHONPATH}/utils/translate_squad_from_base.py ${DATAPATH}/squad/base_v1.1/dev-v1.1_${lang}_base_n.json ${lang} ${BASEPATH}/matcher_exp/enq_25_12_2022/train_matcher_${lang} --output_dir ${DATAPATH}/squad/enq_25_12_2022/ --original_dataset_path ${DATAPATH}/squad/dev-v1.1.json --match_thresh 0.05 --from_en --both;
 done
