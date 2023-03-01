0d5e6e5354838283f94a319af149aa4bc5dfb84e
ofri masad <ofri@deci.ai>
2023-03-01 13:59:08 +0200

    added languages

--- src/languages/__init__.py
+++ src/languages/__init__.py
@@ -1,9 +1,15 @@
 from languages.arabic import Arabic
+from languages.bengali import Bengali
 from languages.chinese import Chinese
+from languages.czech import Czech
+from languages.dutch import Dutch
+from languages.finnish import Finnish
+from languages.french import French
 from languages.german import German
 from languages.greek import Greek
 from languages.hindi import Hindi
 from languages.russian import Russian
+from languages.swedish import Swedish
 from languages.thai import Thai
 from languages.turkish import Turkish
 from languages.vietnamese import Vietnamese
@@ -12,7 +18,8 @@ from languages.hebrew import Hebrew
 from languages.persian import Persian
 from languages.spanish import Spanish
 
-__all__ = [English, Spanish, Hebrew, Persian, Arabic, Chinese, German, Greek, Hindi, Russian, Thai, Turkish, Vietnamese]
+__all__ = [English, Spanish, Hebrew, Persian, Arabic, Chinese, German, Greek, Hindi, Russian, Thai, Turkish, Vietnamese,
+           French, Swedish, Finnish, Dutch, Czech, Bengali]
 
 LANGUAGES = {}
 
