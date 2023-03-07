9b0ecd92e34a0a1ae364f08543147ab1c953a0f5
ofri masad <ofri@deci.ai>
2023-03-07 21:49:05 +0200

    test nl

---
+++ scripts/train_squad_nl_eval_P_Direkt.sh
@@ -0,0 +1,30 @@
+# batch (total on all devices = 16)
+export BASEPATH=/home/ofri/qa_translate
+export PYTHONPATH=${BASEPATH}/src
+export DATAPATH=${BASEPATH}/data
+export CUDA_VISIBLE_DEVICES=0,1
+
+python ${PYTHONPATH}/train/run_qa.py \
+  --model_name_or_path bert-base-multilingual-cased \
+  --train_file=${DATAPATH}/squad/enq_25_12_2022/train_v1.0hf_nl_0.05_enq_both.json \
+  --validation_file=${DATAPATH}/dutch/P-Direkt_full/pd_qa_1.1_hf.json \
+  --test_file=${DATAPATH}/dutch/P-Direkt_full/pd_qa_1.1_hf.json \
+  --output_dir=${BASEPATH}/exp_xquad_new/train_squad_nl_enq_25_12_2022_test_P_Direkt\
+  --run_name=train_squad_both_matcher_aug_test_xquad_cs \
+  --do_train \
+  --do_eval \
+  --do_predict \
+  --doc_stride=128 \
+  --max_seq_length=384 \
+  --per_gpu_train_batch_size=4 \
+  --gradient_accumulation_steps=8 \
+  --learning_rate=3e-5 \
+  --save_steps=-1 \
+  --num_train_epochs=3.0 \
+  --warmup_steps=500 \
+  --evaluation_strategy steps \
+  --weight_decay=0.0001 \
+  --overwrite_output_dir
+
+
+
